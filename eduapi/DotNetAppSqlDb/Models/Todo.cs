fd7c4eb3e407af6fe5920c238492b495589acc63
Eduardo Rocha <edu_a.r@hotmail.com>
2020-10-04 19:02:39 -0300

    Tudo

---
+++ eduapi/DotNetAppSqlDb/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+    }
+}
